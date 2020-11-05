3b41550dc0377e56213d5e414f8dcf221eaafd14
Takuhei Mizutani <takuheim1011@gmail.com>
2020-11-05 20:55:18 +0900

    added colors

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,3 +10,13 @@ parsed['drinks'].each do |thing|
   Ingredient.create!(name: name)
 end
 puts "generated #{Ingredient.count} ingredients"
+puts "clearing cocktails"
+Cocktail.destroy_all
+puts "drank all cocktails..."
+puts "creating yummy cocktails"
+20.times do
+  url = open('https://www.thecocktaildb.com/api/json/v1/1/random.php').read
+  parsed = JSON.parse(url)
+  Cocktail.create!(name: parsed['drinks'].first['strDrink'])
+end
+puts "created #{Cocktail.count} cocktails!"
