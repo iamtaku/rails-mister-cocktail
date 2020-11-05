f5c7c1865ed8a6de43405d91c537ca48ff975c99
Takuhei Mizutani <takuheim1011@gmail.com>
2020-11-05 13:12:30 +0900

    seeded ingredients

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts "clearing ingredients..."
+Ingredient.destroy_all
+puts "cleared ingredients..."
+puts "generating ingredients..."
+
+list = open('https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list').read
+parsed = JSON.parse(list)
+parsed['drinks'].each do |thing|
+  name = thing['strIngredient1']
+  Ingredient.create!(name: name)
+end
+puts "generated #{Ingredient.count} ingredients"
