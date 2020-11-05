badc0d199c04da45e90c7af7d2a71c1ff92a45b0
Takuhei Mizutani <takuheim1011@gmail.com>
2020-11-05 15:11:12 +0900

    finnished routes

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -6,6 +6,7 @@ class CocktailsController < ApplicationController
   end
 
   def show
+    @doses = @cocktail.doses
   end
 
   def new
---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,33 @@
+class DosesController < ApplicationController
+  before_action :set_cocktail, only: [:create, :new]
+  def new
+    @dose = Dose.new
+  end
+
+  def create
+    @dose = Dose.new(set_dose)
+    @dose.cocktail = @cocktail
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+    redirect_to cocktail_path(@dose.cocktail)
+  end
+
+  private
+
+  def set_cocktail
+    @cocktail = Cocktail.find(params[:cocktail_id])
+  end
+
+  def set_dose
+    params.require(:dose).permit(:description, :ingredient_id)
+  end
+
+end
