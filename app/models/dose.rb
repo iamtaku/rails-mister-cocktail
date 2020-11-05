fd1792a9f9864005dda43cee3d51425a3911bb27
Takuhei Mizutani <takuheim1011@gmail.com>
2020-11-05 11:55:56 +0900

    finished model associations + validations

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,4 +1,6 @@
 class Dose < ApplicationRecord
-  belongs_to :ingredient
-  belongs_to :cocktail
+ belongs_to :cocktail
+ belongs_to :ingredient
+ validates :description, presence: true
+ validates :cocktail, uniqueness: { scope: :ingredient }
 end
